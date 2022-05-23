c69f7f261164305478ad80c5adf51ec57c6d6ac6
andydangVN <dangminhnghia@gmail.com>
2022-05-23 12:02:40 +0700

    if statement

---
+++ dice.js
@@ -0,0 +1,20 @@
+let x = Math.floor(Math.random()*6) +1
+let image1= "dice" + x + ".png"
+document.querySelectorAll("img")[0].setAttribute("src", "image/"+image1)
+
+let y = Math.floor(Math.random()*6) +1
+let image2= "dice" + y + ".png"
+document.querySelectorAll("img")[1].setAttribute("src", "image/"+image2)
+
+if (x>y){
+    document.querySelector("h1").innerHTML = "Player 1 win";
+}
+
+if (x<y)
+{
+    document.querySelector("h1").innerHTML = "Player 2 win";
+}
+if (x=y)
+{
+    document.querySelector("h1").innerHTML = "Draw";
+}
